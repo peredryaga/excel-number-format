f1e50c2e25a1c75bbbb3d811e4e8afc9ac4c433b
peredryaga <orlovgb@mail.ru>
2019-05-10 17:26:19 +0300

    Removed functions test_split_tokens_by_parts and test_dispatch_part_type

--- formatcode/converter/utils.py
+++ formatcode/converter/utils.py
@@ -2,54 +2,12 @@
 
 from __future__ import division, print_function, unicode_literals
 
-from collections import defaultdict
 
-from formatcode.converter.errors import ConditionError, PartsCountError, PartTypeError
-from formatcode.converter.parts import (DateFormat, EmptyFormat, FloatFormat, IntegerFormat, StringFormat,
-                                        TimeDeltaFormat)
-from formatcode.lexer.tokens import (BlockDelimiter, ConditionToken, DateTimeToken, DigitToken, DotDelimiter,
-                                     StringSymbol, TimeDeltaToken)
-
-
-def split_tokens_by_parts(tokens):
-    counter = 1
-
-    part = []
+def split_tokens(tokens, separator):
+    out = [[]]
     for token in tokens:
-        if isinstance(token, BlockDelimiter):
-            if counter == 4:
-                # There can be only 4 parts
-                raise PartsCountError(tokens)
-            yield part
-            counter += 1
-            part = []
+        if isinstance(token, separator):
+            out.append([])
         else:
-            if isinstance(token, ConditionToken) and counter > 2:
-                # Only 1 or 2 block can have the condition
-                raise ConditionError(tokens)
-
-            part.append(token)
-    yield part
-
-
-def dispatch_part_type(tokens):
-    flags = defaultdict(bool)
-
-    for token in tokens:
-        for flag in (DigitToken, DateTimeToken, TimeDeltaToken, StringSymbol, DotDelimiter):
-            if isinstance(token, flag):
-                flags[flag] = True
-                break
-
-    if flags[DigitToken] and flags[DateTimeToken]:
-        raise PartTypeError(tokens)
-    elif flags[TimeDeltaToken]:
-        return TimeDeltaFormat
-    elif flags[DigitToken]:
-        return FloatFormat if flags[DotDelimiter] else IntegerFormat
-    elif flags[DateTimeToken]:
-        return DateFormat
-    elif flags[StringSymbol]:
-        return StringFormat
-    else:
-        return EmptyFormat
+            out[-1].append(token)
+    return out
--- tests/test_converter/test_utils.py
+++ tests/test_converter/test_utils.py
@@ -5,57 +5,20 @@ from __future__ import division, print_function, unicode_literals
 import pytest
 
 from formatcode.converter.errors import ConditionError, PartsCountError
-from formatcode.converter.parts import (DateFormat, EmptyFormat, FloatFormat, IntegerFormat, StringFormat,
-                                        TimeDeltaFormat)
-from formatcode.converter.utils import dispatch_part_type, split_tokens_by_parts
+from formatcode.converter.utils import split_tokens
 from formatcode.lexer.tokens import (BlockDelimiter, ColorToken, ConditionToken, DateTimeToken, DotDelimiter,
                                      StringSymbol, TimeDeltaToken, ZeroToken)
 
 
-def test_split_tokens_by_parts():
+def test_split_tokens():
     zero = ZeroToken('0')
+    dot = DotDelimiter('.')
     delimiter = BlockDelimiter(';')
     condition = ConditionToken('[>100]')
     color = ColorToken('[Blue]')
 
-    assert list(split_tokens_by_parts([zero, delimiter, zero])) == [[zero], [zero]]
-    assert list(split_tokens_by_parts([color, zero, delimiter, zero])) == [[color, zero], [zero]]
-    assert list(split_tokens_by_parts([condition, zero, delimiter, condition, zero])) == [[condition, zero],
-                                                                                          [condition, zero]]
-
-    with pytest.raises(ConditionError):
-        list(split_tokens_by_parts([color, delimiter, zero, delimiter, condition, zero]))
-
-    with pytest.raises(PartsCountError):
-        list(split_tokens_by_parts([zero, delimiter, zero, delimiter, zero, delimiter, zero, delimiter, zero]))
-
-
-def test_dispatch_part_type():
-    zero = ZeroToken('0')
-    date = DateTimeToken('yy')
-    timedelta = TimeDeltaToken('[h]')
-    dot = DotDelimiter('.')
-    condition = ConditionToken('[>100]')
-    color = ColorToken('[Blue]')
-    string = StringSymbol('"hello"')
-
-    assert dispatch_part_type([zero, zero]) == IntegerFormat
-    assert dispatch_part_type([color, zero, zero]) == IntegerFormat
-    assert dispatch_part_type([string, zero, zero]) == IntegerFormat
-
-    assert dispatch_part_type([zero, dot, zero]) == FloatFormat
-    assert dispatch_part_type([color, condition, zero, dot, zero]) == FloatFormat
-    assert dispatch_part_type([zero, dot, zero, string]) == FloatFormat
-
-    assert dispatch_part_type([string]) == StringFormat
-    assert dispatch_part_type([color, string]) == StringFormat
-
-    assert dispatch_part_type([date]) == DateFormat
-    assert dispatch_part_type([color, date]) == DateFormat
-
-    assert dispatch_part_type([timedelta]) == TimeDeltaFormat
-    assert dispatch_part_type([color, timedelta]) == TimeDeltaFormat
-    assert dispatch_part_type([color, timedelta, dot, zero]) == TimeDeltaFormat
-
-    assert dispatch_part_type([]) == EmptyFormat
-    assert dispatch_part_type([color]) == EmptyFormat
+    assert split_tokens([zero, delimiter, zero], BlockDelimiter) == [[zero], [zero]]
+    assert split_tokens([zero, dot, zero], DotDelimiter) == [[zero], [zero]]
+    assert split_tokens([color, zero, delimiter, zero], BlockDelimiter) == [[color, zero], [zero]]
+    assert split_tokens([condition, zero, delimiter, condition, zero], BlockDelimiter) == [[condition, zero],
+                                                                                           [condition, zero]]
